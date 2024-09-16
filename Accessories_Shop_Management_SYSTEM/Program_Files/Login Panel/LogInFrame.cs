b3b3355e82f383f76609ce533bfad21ff18e9c5c
Munna <habiburmunna0@gmail.com>
2024-09-17 00:29:38 +0600

    first commit

---
+++ Accessories_Shop_Management_SYSTEM/Program_Files/Login Panel/LogInFrame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_Files.Login_Panel
+{
+    public partial class LogInFrame : Form
+    {
+        public LogInFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
